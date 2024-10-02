e4442bd4e5ee9c7e212f836c7bad1326e7c4d95a
Chloé <chloe-1903@live.fr>
2024-10-02 11:16:46 +0200

    Update page.tsx

--- app/welcome/page.tsx
+++ app/welcome/page.tsx
@@ -4,7 +4,7 @@ import {AnimatedImage} from '@/components/animatedImage/animatedImage';
 export default async function WelcomePage() {
     return (
         <>
-            <h1 className="text-3xl">Here is a cat!</h1>
+            <h1 className="text-2xl">Here is a cat!</h1>
             <AnimatedImage path="https://cataas.com/cat/funny"/>
         </>
     );
